9ad39ecd13e96f160b75527031ba5a88a6d72082
Vitali Karpuk <karpuk-job@yandex.ru>
2019-10-06 12:36:42 +0300

    test

---
+++ send.php
@@ -0,0 +1,14 @@
+<?php
+    $fio = $_POST['fio'];
+    $email = $_POST['email'];
+    echo $fio;
+echo "<br>";
+echo $email;
+mail("karpuk-job@yandex.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n");
+if (mail("example@mail.ru", "Заказ с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n"))
+ {
+    echo "сообщение успешно отправлено";
+} else {
+    echo "при отправке сообщения возникли ошибки";
+}
+?>
