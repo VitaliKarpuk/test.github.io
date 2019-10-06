00590e9d5118977fba54b82571f7d5fc1fffe849
Vitali Karpuk <karpuk-job@yandex.ru>
2019-10-06 13:25:59 +0300

    test

--- mail.php
+++ mail.php
@@ -1,32 +1,26 @@
-<html>
-<head>
-<title>Форма заявки с сайта</title>
-</head>
-<body>
 <?php
-//проверяем, существуют ли переменные в массиве POST
-if(!isset($_POST['fio']) and !isset($_POST['email'])){
- ?> <form action="send.php" method="post">
-<input type="text" name="fio" placeholder="Укажите ФИО" required>
-<input type="text" name="email" placeholder="Укажите e-mail" required>
-<input type="submit" value="Отправить">
-</form> <?php
-} else {
- //показываем форму
- $fio = $_POST['fio'];
- $email = $_POST['email'];
- $fio = htmlspecialchars($fio);
- $email = htmlspecialchars($email);
- $fio = urldecode($fio);
- $email = urldecode($email);
- $fio = trim($fio);
- $email = trim($email);
- if (mail("example@mail.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n")){
- echo "Сообщение успешно отправлено";
- } else {
- echo "При отправке сообщения возникли ошибки";
- }
+$to = "kurpuk-job@yandex.ru"; // емайл получателя данных из формы 
+$tema = "Форма обратной связи kontakt.ormedia.by"; // тема полученного емайла 
+$message = "Ваше имя : ".$_POST['uname']."<br>";//присвоить переменной значение, полученное из формы name=name
+$message .= "Номер телефона: ".$_POST['unumber']."<br>"; //полученное из формы name=phone
+$headers  = 'MIME-Version: 1.0' . "\r\n"; // заголовок соответствует формату плюс символ перевода строки
+  $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n"; // указывает на тип посылаемого контента
+ //отправляет получателю на емайл значения переменных
+
+if (empty($bezspama)) /* Оценка поля bezspama - должно быть пустым*/
+{
+/* Отправляем сообщение, используя mail() функцию */
+$from  = "From: $name <$email> \r\n Reply-To: $email \r\n";
+if (mail($to, $tema, $message, $headers)) {
+    header('Refresh: 5; URL=http://kontakt.ormedia.by/');
+    echo '
+    
+    Письмо отправлено, через 5 секунд вы вернетесь на страницу';}
+else {
+    header('Refresh: 2; URL=http://kontakt.ormedia.by/');
+    echo '
+    
+    Письмо не отправлено, через 5 секунд вы вернетесь на страницу YYY';}
 }
+exit; /* Выход без сообщения, если поле bezspama заполнено спам ботами */
 ?>
-</body>
-</html>
